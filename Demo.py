8a95874cb326949d2fc5dbd21f81d9c816e5bdaa
GaryHou8 <GaryHou79@outlook.com>
2021-12-16 11:16:48 +0800

    Add files via upload

---
+++ Demo.py
@@ -0,0 +1,34 @@
+import numpy as np
+from scipy import signal
+import pandas as pd
+from mne.decoding import CSP
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+
+def train_eeg():
+
+    file_id = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20']
+    predict_label = []
+    trial_id = [i for i in range(1, 1601)]
+    for i in range(20):
+        train_data = np.load('train_data_' + file_id[i] + '.npy')
+        train_label = np.load('train_label_' + file_id[i] + '.npy')
+        test_data = np.load('test_data_' + file_id[i] + '.npy')
+
+        b, a = signal.butter(8, [8 / 125, 30 / 125], 'bandpass')
+        train_data = signal.filtfilt(b, a, train_data)
+        test_data = signal.filtfilt(b, a, test_data)
+
+        csp = CSP(n_components=20, reg=None, log=False, norm_trace=False)
+        svm = SVC(kernel='linear')
+        clf = Pipeline([('CSP', csp), ('SVM', svm)])
+        clf.fit(train_data, train_label)
+        result = clf.predict(test_data)
+        predict_label.extend(result)
+
+    dataframe = pd.DataFrame({'TrialId': trial_id,
+                              'Label': predict_label})
+    dataframe.to_csv("sample_submission1.csv", index=False, sep=',')
+
+if __name__ == '__main__':
+    train_eeg()
